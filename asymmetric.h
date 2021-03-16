d6122bcb06738f29535d0caaff09eec26c803d09
NilEis <52820950+NilEis@users.noreply.github.com>
2021-03-16 18:15:28 +0100

    refactoring

---
+++ asymmetric.h
@@ -0,0 +1,6 @@
+#ifndef ASYMMETRIC_H_INCLUDED
+#define ASYMMETRIC_H_INCLUDED
+
+void get_RSA(int p,int q, int *ret_N, int *ret_e, int *ret_d);
+
+#endif // ASYMMETRIC_H_INCLUDED
---
+++ helper_functions.h
@@ -0,0 +1,8 @@
+#ifndef HELPER_FUNCTIONS_H_INCLUDED
+#define HELPER_FUNCTIONS_H_INCLUDED
+
+int is_prime(int p);
+
+void exit_program(int code);
+
+#endif // HELPER_FUNCTIONS_H_INCLUDED
