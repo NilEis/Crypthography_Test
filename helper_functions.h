0f752f06c94fec52634d7e56b0b9db95db77e3b8
NilEis <52820950+NilEis@users.noreply.github.com>
2021-03-16 18:29:47 +0100

    Update helper_functions.h

--- helper_functions.h
+++ helper_functions.h
@@ -1,7 +1,20 @@
 #ifndef HELPER_FUNCTIONS_H_INCLUDED
 #define HELPER_FUNCTIONS_H_INCLUDED
 
+/** \brief Tests whether a number is a prime number
+ *
+ * \param p is the number to be tested
+ * \return A 0 if the number is a prime number, otherwise a -1
+ *
+ */
+
 int is_prime(int p);
+
+/** \brief Exits the program after waiting for the user's confirmation
+ *
+ * \param code is the error code with which the programme is terminated
+ *
+ */
 
 void exit_program(int code);
 
