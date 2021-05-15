5e1b254e6e2a3045c84bfb0125792f15b4b510d9
NilEis <52820950+NilEis@users.noreply.github.com>
2021-05-15 16:11:42 +0200

    Update asymmetric.h

--- Include/asymmetric.h
+++ Include/asymmetric.h
@@ -1,13 +1,7 @@
-#ifdef __cplusplus
-extern "C" {
-#endif // __cplusplus
-#ifndef ASYMMETRIC_H_INCLUDED
-#define ASYMMETRIC_H_INCLUDED
+#ifndef RSA_H_INCLUDED
+#define RSA_H_INCLUDED
 #include <stdint.h>
 
 void get_RSA(uint32_t p,uint32_t q, uint32_t *ret_N, uint32_t *ret_e, uint32_t *ret_d);
 
-#endif // ASYMMETRIC_H_INCLUDED
-#ifdef __cplusplus
-}
-#endif // __cplusplus
+#endif // RSA_H_INCLUDED
